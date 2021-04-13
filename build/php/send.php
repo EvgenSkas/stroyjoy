4fed6de99a535794754a06bce01c09f3d5796db5
yskaskevich <yskaskevich@eisgroup.com>
2021-04-13 08:50:38 +0300

    Fix send.php and change photos

--- build/php/send.php
+++ build/php/send.php
@@ -1,19 +1,24 @@
 <?
-        $to = "evgenskas@gmail.com";
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+    $to = "evgenskas@gmail.com";
+    $subject = 'Обратный звонок'; //Заголовок сообщения
+    $message = '
+            <html>
+                <head>
+                    <title>'.$subject.'</title>
+                </head>
+                <body>
+                    <p>Имя: '.$_POST['name'].'</p>
+                    <p>Email: '.$_POST['email'].'</p>
+                    <p>Телефон: '.$_POST['phone'].'</p>                                   
+                </body>
+            </html>'; //Текст сообщения
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+    $headers .= "From: Отправитель <skaskevich23@gmail.com>\r\n"; //Наименование и почта отправителя
+    $send = mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+    if($send) {
+        echo "Сообщение отправлено! Скоро мы с вами свяжемся."
     }
-        $subject = 'Обратный звонок'; //Заголовок сообщения
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Email: '.$_POST['email'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>                                   
-                    </body>
-                </html>'; //Текст сообщения
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 ?>
